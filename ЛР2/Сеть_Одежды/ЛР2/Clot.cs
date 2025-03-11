a0b4f5cb269a7d9ac22bae8c80ad717ea279e53d
Andrey-Shelgunov <selgunova3@gmail.com>
2025-03-11 09:02:09 +0300

    Добавил ЛР2

---
+++ ЛР2/Сеть_Одежды/ЛР2/Clot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР2
+{
+    public enum Clothes
+    {
+        sports_shiirt = 1,
+        trousers,
+        fur,
+        skirt
+    }
+}
---
+++ ЛР2/Сеть_Одежды/ЛР2/Clothe_Spisok.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР2
+{
+    public class Clothes_spisok
+    {
+        private double price;
+        private string name;
+
+        public void Setnames(string M)
+        {
+            name = M;
+        }
+        public string getvaluesstring()
+        {
+            return name;
+        }
+        public void Setprice(double P)
+        {
+            price = P;
+        }
+        public Clothes_spisok(double MOR, string POK)
+        {
+            price = MOR;
+            name = POK;
+        }
+    }
+}
