5ad99a66283527404d686d356de67af95c299aa5
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-03-13 10:36:37 +0300

    Реализовать приват TreeNode

--- TheeView/MyTreee/MyTreeView/Form1.Designer.cs
+++ TheeView/MyTreee/MyTreeView/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(251, 152);
             this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // Form1
             // 
--- TheeView/MyTreee/MyTreeView/Form1.cs
+++ TheeView/MyTreee/MyTreeView/Form1.cs
@@ -13,69 +13,67 @@ namespace MyTreeView
     public partial class Form1 : Form
     {
         public Form1()
-
         {
             InitializeComponent();
             PopulateTreeView();
         }
-            private object selectedNodeTextBox;
-    
-        public static object Nodes { get; private set; }
+
+       
+        private TextBox selectedNodeTextBox;
+        private static readonly object Nodes;
+        private TreeNode AddNode(string v, object nodes)
+        {
+            throw new NotImplementedException();
+        }
+
         private void PopulateTreeView()
         {
             // Главный узел: Жанры
-            TreeNode genresNode = new TreeNode("Жанры");
-            Form1.Nodes.Add(genresNode);
+            TreeNode genresNode = AddNode("Жанры", Form1.Nodes);
 
             // Жанр: Стратегии
-            TreeNode strategyNode = new TreeNode("Стратегии");
-            genresNode.Nodes.Add(strategyNode);
+            TreeNode strategyNode = AddNode("Стратегии", genresNode.Nodes);
 
             // Игры в жанре Стратегии
-            strategyNode.Nodes.Add(new TreeNode("Civilization VI"));
-            strategyNode.Nodes.Add(new TreeNode("StarCraft II"));
-            strategyNode.Nodes.Add(new TreeNode("Crusader Kings III"));
+            AddNode("Civilization VI", strategyNode.Nodes);
+            AddNode("StarCraft II", strategyNode.Nodes);
+            AddNode("Crusader Kings III", strategyNode.Nodes);
 
             // Жанр: RPG (Ролевые игры)
-            TreeNode rpgNode = new TreeNode("RPG");
-            genresNode.Nodes.Add(rpgNode);
+            TreeNode rpgNode = AddNode("RPG", genresNode.Nodes);
 
             // Игры в жанре RPG
-            rpgNode.Nodes.Add(new TreeNode("The Witcher 3: Wild Hunt"));
-            rpgNode.Nodes.Add(new TreeNode("Elden Ring"));
-            rpgNode.Nodes.Add(new TreeNode("Cyberpunk 2077"));
+            AddNode("The Witcher 3: Wild Hunt", rpgNode.Nodes);
+            AddNode("Elden Ring", rpgNode.Nodes);
+            AddNode("Cyberpunk 2077", rpgNode.Nodes);
 
             // Жанр: Шутеры
-            TreeNode shooterNode = new TreeNode("Шутеры");
-            genresNode.Nodes.Add(shooterNode);
+            TreeNode shooterNode = AddNode("Шутеры", genresNode.Nodes);
 
             // Игры в жанре Шутеры
-            shooterNode.Nodes.Add(new TreeNode("Counter-Strike: Global Offensive"));
-            shooterNode.Nodes.Add(new TreeNode("Valorant"));
-            shooterNode.Nodes.Add(new TreeNode("Call of Duty: Warzone"));
+            AddNode("Counter-Strike: Global Offensive", shooterNode.Nodes);
+            AddNode("Valorant", shooterNode.Nodes);
+            AddNode("Call of Duty: Warzone", shooterNode.Nodes);
 
 
             // Главный узел: Платформы
-            TreeNode platformsNode = new TreeNode("Платформы");
-            Form1.Nodes.Add(platformsNode);
+            TreeNode platformsNode = AddNode("Платформы", Form1.Nodes);
 
             // Платформа: PC
-            TreeNode pcNode = new TreeNode("PC");
-            platformsNode.Nodes.Add(pcNode);
+            TreeNode pcNode = AddNode("PC", platformsNode.Nodes);
 
             // Игры на PC (можно добавить те же игры, что и в жанрах, это пример)
-            pcNode.Nodes.Add(new TreeNode("Civilization VI"));
-            pcNode.Nodes.Add(new TreeNode("The Witcher 3: Wild Hunt"));
-            pcNode.Nodes.Add(new TreeNode("Counter-Strike: Global Offensive"));
+            AddNode("Civilization VI", pcNode.Nodes);
+            AddNode("The Witcher 3: Wild Hunt", pcNode.Nodes);
+            AddNode("Counter-Strike: Global Offensive", pcNode.Nodes);
 
             // Платформа: PlayStation
-            TreeNode playStationNode = new TreeNode("PlayStation");
-            platformsNode.Nodes.Add(playStationNode);
+            TreeNode playStationNode = AddNode("PlayStation", platformsNode.Nodes);
 
             // Игры на PlayStation
-            playStationNode.Nodes.Add(new TreeNode("The Last of Us Part II"));
-            playStationNode.Nodes.Add(new TreeNode("Spider-Man: Miles Morales"));
-            playStationNode.Nodes.Add(new TreeNode("God of War Ragnarök"));
+            AddNode("The Last of Us Part II", playStationNode.Nodes);
+            AddNode("Spider-Man: Miles Morales", playStationNode.Nodes);
+            AddNode("God of War Ragnarök", playStationNode.Nodes);
 
 
             // Разворачиваем дерево для удобства просмотра
@@ -83,16 +81,29 @@ namespace MyTreeView
             platformsNode.Expand();
         }
 
+        
+
+        // Вспомогательный метод для добавления узлов
+        private TreeNode AddNode(string text, TreeNodeCollection nodes)
+        {
+            TreeNode node = new TreeNode(text);
+            nodes.Add(node);
+            return node; // Возвращаем добавленный узел, чтобы можно было сразу добавлять дочерние узлы.
+        }
+
         private void gameTreeView_AfterSelect(object sender, TreeViewEventArgs e)
         {
             // Обработчик события выбора узла
-            if (e.Node != null)
+            if (e.Node != null && selectedNodeTextBox != null) // Важно: проверка на null!
             {
-                // Выводим текст выбранного узла в TextBox (можно сделать что-то другое)
                 selectedNodeTextBox.Text = e.Node.Text;
             }
         }
 
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
         }
+    }
 }
 
