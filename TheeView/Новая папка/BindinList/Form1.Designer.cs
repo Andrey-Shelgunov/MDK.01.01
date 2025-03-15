b6fbb51f43f1d752bbd4eb09bbd9602c7c462fa4
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-03-15 09:25:46 +0300

    Добавили новыйц проект для ввода в таблицу

---
+++ TheeView/Новая папка/BindinList/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BindinList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dobavit = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.redakt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(478, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dobavit
+            // 
+            this.dobavit.Location = new System.Drawing.Point(153, 324);
+            this.dobavit.Name = "dobavit";
+            this.dobavit.Size = new System.Drawing.Size(75, 23);
+            this.dobavit.TabIndex = 1;
+            this.dobavit.Text = "Добавить";
+            this.dobavit.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(321, 323);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // redakt
+            // 
+            this.redakt.Location = new System.Drawing.Point(511, 324);
+            this.redakt.Name = "redakt";
+            this.redakt.Size = new System.Drawing.Size(75, 23);
+            this.redakt.TabIndex = 3;
+            this.redakt.Text = "Редактировать";
+            this.redakt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.redakt);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.dobavit);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button dobavit;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button redakt;
+    }
+}
+
---
+++ TheeView/Новая папка/BindinList/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindinList
+{
+    class Student
+    {
+        public string name_ { get; set; }
+        public string Subname_ { get; set; }
+        public int age_ { get; set;}
+    }
+}
